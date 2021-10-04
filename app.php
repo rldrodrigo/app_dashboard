f1d4620f487cc9791bd47323fcbe41063867c454
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-04 16:28:24 -0300

    Inserção de dados do back end no front
    Usando ajax foi inserido os valores de numero de vendas e total vendas a partir do mês

--- app.php
+++ app.php
@@ -88,9 +88,20 @@ $dashboard = new Dashboard();
 
 $conexao = new Conexao();
 
-$dashboard->__set('data_inicio', '2018-08-01');
-$dashboard->__set('data_fim', '2018-08-31');
+$competencia = explode('-', $_GET['competencia']);
+$ano = $competencia[0];
+$mes = $competencia[1];
+
+$dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+
+$dashboard->__set('data_inicio', $ano . '-' . $mes . '-' . '-01');
+$dashboard->__set('data_fim', $ano . '-' . $mes . '-' . $dias_do_mes);
 
 $bd = new Bd($conexao, $dashboard);
 $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
 $dashboard->__set('totalVendas', $bd->getTotalVendas());
+
+echo json_encode($dashboard);
+
+
+//Implementar o restante do Dashboard
--- script.js
+++ script.js
@@ -11,5 +11,25 @@ $(document).ready(() => {
 
     })
 
+    //ajax
+    $('#competencia').on('change', e => {
+
+        let competencia = $(e.target).val()
+
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`, // x-www-fomr-urlencoded
+            dataType: 'json',
+            success: dados => {
+                $('#numeroVendas').html(dados.numeroVendas)
+                $('#totalVendas').html('R$ ' + dados.totalVendas)
+            },
+            error: erro => { console.log(erro) }
+        })
+
+        // Método, url, dados, erro
+    })
+
 
 })
